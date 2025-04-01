6c2b883a19f257664be907ccb2d3046ca44c66e2
NikolayO <n9056446438@gmail.com>
2025-04-01 23:29:50 +0300

    Main and Employee created. Task-basic.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,142 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+// Stuff book
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.Arrays;
+
+public class Main {
+
+    protected static Employee[] storage = new Employee[10];
+
+    public static void main(String[] args) {
+        storage[0] = new Employee("Ben", "Gun", "Yoshkovich", "2", 10.5F);
+        storage[2] = new Employee("Gans", "Grueber", "", "1", 100.0F);
+//        storage[3] = new Employee("Josef", "Barbera", "", "3", -1.0F);
+        storage[6] = new Employee("Devi", "Jons", "", "2", 10.5F);
+        storage[9] = new Employee("Mike", "Vazovski", "", "3", 100.0F);
+
+        // Requirements Basic
+        System.out.println("Список всех сотрудников со всеми данными:");
+        printEmployees();
+        System.out.println("Список ФИО всех сотрудников:");
+        printEmployeesFullNames();
+        System.out.println("Сумма затрат на ЗП: " + getSumOfSalaries());
+        System.out.println("Поиск сотрудника с минимальной ЗП: " + Arrays.toString(getEmployeesWithMinimumSalary()));
+        System.out.println("Поиск сотрудника с максимальной ЗП: " + Arrays.toString(getEmployeesWithMaximumSalary()));
+        System.out.println("Подсчет среднего значения ЗП: " + getAverageSalary());
+    }
+
+    protected static void printEmployees() {
+        for (Employee employee : storage) {
+            if (employee != null) {
+                System.out.println(employee.toString());
+            }
         }
+    }
+
+    protected static void printEmployeesFullNames() {
+        for (Employee employee : storage) {
+            if (employee != null) {
+                System.out.println(employee.fullName());
+            }
+        }
+    }
+
+    protected static float getSumOfSalaries() {
+        float sumOfSalary = 0;
+
+        for (Employee employee : storage) {
+            if (employee != null) {
+                sumOfSalary += employee.getSalary();
+            }
+        }
+
+        return sumOfSalary;
+    }
+
+    protected static Employee[] getEmployeesWithMinimumSalary() {
+        float minimumSalary = 0;
+        Employee[] found = null;
+
+        for (Employee employee : storage) {
+            if (employee == null) {
+                continue;
+            }
+
+            if (minimumSalary > employee.getSalary() || minimumSalary == 0) {
+                minimumSalary = employee.getSalary();
+            }
+        }
+
+        for (Employee employee : storage) {
+            if (employee == null) {
+                continue;
+            }
+
+            if (minimumSalary != employee.getSalary()) {
+                continue;
+            }
+
+            if (found == null) {
+                found = new Employee[1];
+                found[0] = employee;
+            } else {
+                found = Arrays.copyOf(found, found.length + 1);
+                found[found.length - 1] = employee;
+            }
+        }
+
+        return found;
+    }
+
+    protected static Employee[] getEmployeesWithMaximumSalary() {
+        float maximumSalary = 0;
+        Employee[] found = null;
+
+        for (Employee employee : storage) {
+            if (employee == null) {
+                continue;
+            }
+
+            if (maximumSalary < employee.getSalary() || maximumSalary == 0) {
+                maximumSalary = employee.getSalary();
+            }
+        }
+
+        for (Employee employee : storage) {
+            if (employee == null) {
+                continue;
+            }
+
+            if (maximumSalary != employee.getSalary()) {
+                continue;
+            }
+
+            if (found == null) {
+                found = new Employee[1];
+                found[0] = employee;
+            } else {
+                found = Arrays.copyOf(found, found.length + 1);
+                found[found.length - 1] = employee;
+            }
+        }
+
+        return found;
+    }
+
+    protected static float getAverageSalary() {
+        float averageSalary = 0;
+        int countEmploees = 0;
+
+        for (Employee employee : storage) {
+            if (employee != null) {
+                countEmploees++;
+                averageSalary += employee.getSalary();
+            }
+        }
+
+        if (countEmploees > 0) {
+            averageSalary = averageSalary / countEmploees;
+        }
+
+        return averageSalary;
     }
 }
